36599bc799094a6271dde9986dcf87880337b765
Lloyd.Dhliwayo <Lloyd.Dhliwayo@lloyd-cumii-lp.econetzw.local>
2022-03-03 20:18:54 +0200

    Save and delete operations functional Table auto resize configured Added click table populate fields Added search Button Update button function added Clear bUTTON Added
    SQL Exception message box added
    Added row count
    row count code commented out
    Entire_Commit_NoLogin

---
+++ src/voters/registration/SqlConnection.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voters.registration;
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author Lloyd.Dhliwayo
+ */
+public class SqlConnection {
+    
+
+    
+}
---
+++ src/voters/registration/VotersRegistration.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voters.registration;
+
+/**
+ *
+ * @author Lloyd.Dhliwayo
+ */
+public class VotersRegistration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Voters voterreg = new Voters();
+      voterreg.show();
+    }
+    
+}
